0d69e56f506dbaa5d4e6273e65d92c08e57fda79
Leonardo Gottardi <leomgottardi@gmail.com>
2021-07-28 18:51:34 -0300

    feat: adicionando diretiva para conseguir cadastrar telefone junto com contato

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
   end
 
   def show
-    render json: @contact
+    render json: @contact, include: [:kind, :phones]
   end
 
   def create
@@ -38,6 +38,12 @@ class ContactsController < ApplicationController
     end
 
     def contact_params
-      params.require(:contact).permit(:name, :email, :birthdate, :kind_id)
+      params.require(:contact).permit(
+                                      :name, 
+                                      :email, 
+                                      :birthdate, 
+                                      :kind_id, 
+                                      phones_attributes: [:number]
+                                    )
     end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,17 @@
 class Contact < ApplicationRecord 
   belongs_to :kind
+  
   has_many :phones
+  accepts_nested_attributes_for :phones
 
   def kind_description 
     self.kind.description
   end
 
   def as_json(options={})
-    super(root:true,
-    methods: [:kind_description])
+    super({
+      root:true,
+      methods: [:kind_description]
+    }.merge(options))
   end
 end
